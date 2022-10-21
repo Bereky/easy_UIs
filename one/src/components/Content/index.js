295433bd07a503c850f48d119e619a99aebede05
bereky <bereketlemma4@gmail.com>
2022-10-22 00:33:39 +0300

    added cards component

--- one/src/components/Content/index.js
+++ one/src/components/Content/index.js
@@ -1,9 +1,11 @@
+import Cards from "./Cards";
 import HeroText from "./HeroText";
 
 const Content = () => {
     return ( 
         <section class="flex flex-col col-auto ">
             <HeroText />
+            <Cards />
         </section>
      );
 }
