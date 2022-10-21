1176a9862d1340d3a125bd1739f1573a045499c7
bereky <bereketlemma4@gmail.com>
2022-10-21 23:43:24 +0300

    heading text created

---
+++ one/src/components/Content/HeroText.js
@@ -0,0 +1,10 @@
+const HeroText = () => {
+    return ( 
+        <div class="mt-8 py-8 flex items-center justify-center flex-col">
+            <h1 class="font-bold text-sxlg text-center mx-4">Getting started with Blockchain</h1>
+            <p class="text-lg text-center mx-4">A few resources to learn about blockchain and cryptocurrencies</p>
+        </div>
+     );
+}
+ 
+export default HeroText;
---
+++ one/src/components/Content/index.js
@@ -0,0 +1,11 @@
+import HeroText from "./HeroText";
+
+const Content = () => {
+    return ( 
+        <section class="flex flex-col col-auto ">
+            <HeroText />
+        </section>
+     );
+}
+ 
+export default Content;
