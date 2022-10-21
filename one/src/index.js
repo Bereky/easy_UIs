79cf578d5b532ed1fdafb4b87e1a74cccf9dbd3b
bereky <bereketlemma4@gmail.com>
2022-10-21 23:21:18 +0300

    include css

--- one/src/index.js
+++ one/src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('_app'));
 root.render(<App />);
