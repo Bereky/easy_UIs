e9782ea1ea5f167f47dbee48d02ac699002e4058
bereky <bereketlemma4@gmail.com>
2022-10-22 02:37:51 +0300

    theme applied to the full screen background

--- one/src/components/app/App.js
+++ one/src/components/app/App.js
@@ -1,13 +1,17 @@
+import { useContext } from "react";
 import Content from "../Content";
+import { ThemeContext } from "../Context/ThemeContext";
 import ThemeProvider from "../Context/ThemeProvider";
 import Nav from "../Nav/Nav";
 
 const App = () => {
+    const theme = useContext(ThemeContext)
+
     return ( 
-        <ThemeProvider>
-            <Nav />
-            <Content />
-        </ThemeProvider>
+            <div className={`h-screen min-h-full ${theme.darkMode && 'bg-darkModeBody'}`}>
+                <Nav />
+                <Content />
+            </div>
      );
 }
  
