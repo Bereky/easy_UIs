de6df6dd97ae7892118eae2d61229923c2eac390
bereky <bereketlemma4@gmail.com>
2022-10-21 23:44:23 +0300

    added content component

--- one/src/components/app/App.js
+++ one/src/components/app/App.js
@@ -1,9 +1,11 @@
+import Content from "../Content";
 import Nav from "../Nav/Nav";
 
 const App = () => {
     return ( 
         <div>
             <Nav />
+            <Content />
         </div>
      );
 }
